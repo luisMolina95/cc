c9066aa1bb690fba41c725f6aee8dfd5f877a869
luisMolina95 <luisrodmolina@gmail.com>
2025-03-01 08:18:35 -0600

    ...

--- wasm/main.c
+++ wasm/main.c
@@ -1,7 +1,7 @@
- int somefun(){
-	 return 29;
- }
+void print(char[1000], int, float);
+
 
  int main(int num){
+	print("Diego", 7, 0.5);
 	return num / 7;
  }
--- wasm/main.js
+++ wasm/main.js
@@ -3,8 +3,17 @@
     const wasmS = await fetch("./main.wasm");
     const text = await wasmS.text();
     console.log(text);
-    const wasm = await WebAssembly.instantiateStreaming(fetch("./main.wasm"), { env: {} });
-    const main = wasm.instance.exports.main(77);
-    console.log("Main77:", main);
+    let wasm;
+    wasm = await WebAssembly.instantiateStreaming(fetch("./main.wasm"), {
+        env: {
+            print: (...args) => {
+                const text = new TextDecoder().decode(new Uint8Array(wasm.instance.exports.memory.buffer, args[0], 1000));
+                console.log(new Uint8Array(wasm.instance.exports.memory.buffer, args[2], 10));
+                console.log("text:", text, args);
+            }
+        }
+    });
+    const main = wasm.instance.exports.main(7777);
+    console.log("Main777:", main);
     console.log('Done!');
 })()
