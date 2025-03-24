1d1f35edc1f7fa460aa3a6662eea9f05884d13c0
luisMolina95 <luisrodmolina@gmail.com>
2025-03-24 12:13:01 -0600

    Add more about pointers

--- 7 - Pointers and Memory/linkedList.c
+++ 7 - Pointers and Memory/linkedList.c
@@ -18,42 +18,34 @@ void addRecord(struct listNode **sc, int stuNum, int gr)
     *sc = newNode;
 }
 
+double averageRecord(struct listNode *sc)
+{
+    if (sc == NULL)
+        return 0;
+    int count = 0;
+    double sum = 0;
+    struct listNode *loopPtr = sc;
+    while (loopPtr != NULL)
+    {
+        sum += loopPtr->grade;
+        count++;
+        loopPtr = loopPtr->next;
+    }
+    double average = sum / count;
+    return average;
+}
+
 int main(int argc, char const *argv[])
 {
-    printf("%i\n", NULL == 0);
+
     struct listNode *sc = NULL;
 
-    // struct listNode *node1 = malloc(sizeof(struct listNode));
-    // node1->studentNum = 3;
-    // node1->grade = 3;
-    // node1->next = NULL;
-
-    // struct listNode node2;
-    // node2.studentNum = 1012;
-    // node2.grade = 93;
-
-    // struct listNode node3;
-    // node3.studentNum = 1076;
-    // node3.grade = 85;
-
-    // struct listNode *node4 = malloc(sizeof(struct listNode));
-    // node4->studentNum = 4;
-    // node4->grade = 44;
-
-    // sc = node1;
     addRecord(&sc, 1, 1);
     addRecord(&sc, 2, 2);
-    addRecord(&sc, 4, 4);
+    addRecord(&sc, 3, 3);
     addRecord(&sc, 5, 5);
 
-    struct listNode *tp = sc;
-    while (true)
-    {
-        if (tp == NULL)
-            break;
-        printf("Student #%i: %i\n", tp->studentNum, tp->grade);
-        tp = tp->next;
-    }
+    printf("Result: %d %f", (int)averageRecord(sc), averageRecord(sc));
 
     return 0;
 }
