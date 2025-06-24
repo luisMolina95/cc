a785c3819870c38f4ce11fbb0cd2a641394efe93
luisMolina95 <luisrodmolina@gmail.com>
2025-06-24 08:09:07 -0600

    Corrections

--- 12 - asm linux x64/example.asm
+++ 12 - asm linux x64/example.asm
@@ -2,8 +2,8 @@ section .data
 EXIT_SUCCESS equ 0
 SYS_exit     equ 60
 
-bVar1 db 17
-bVar2 db 9
+bVar1 db 5
+bVar2 db 5
 bResult db 0
 
 section .text
@@ -12,8 +12,6 @@ _start:
     mov al, byte[bVar1]
     add al, byte[bVar2]
     mov byte[bResult], al
-
-last:
     mov rax, SYS_exit
     movzx rdi, byte[bResult]
     syscall
