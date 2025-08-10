63d5300d999252c1878334a0b9c2ce13a382c5e1
luisMolina95 <luisrodmolina@gmail.com>
2025-08-10 15:34:53 -0600

    Save in build folder

--- 16 - compile and flash wavtag nano 3/flash.cmd
+++ 16 - compile and flash wavtag nano 3/flash.cmd
@@ -1,5 +1,5 @@
 @echo off
 avrdude ^
-    -C"C:\Program Files (x86)\Arduino\hardware\tools\avr/etc/avrdude.conf" ^
-    -v -patmega328p -carduino -PCOM7 -b57600 -D ^
-    -Uflash:w:blink.hex:i
+    -C"D:\Program Files (x86)\Arduino\hardware\tools\avr/etc/avrdude.conf" ^
+    -v -patmega328p -carduino -PCOM -b57600 -D ^
+    -Uflash:w:"build\blink.hex":i
