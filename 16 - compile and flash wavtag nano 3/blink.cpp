29f57ef0647f1ccfbf6c222e4bc846d055da672e
luisMolina95 <luisrodmolina@gmail.com>
2025-09-04 13:05:31 -0600

    Add assembly test

--- 16 - compile and flash wavtag nano 3/blink.cpp
+++ 16 - compile and flash wavtag nano 3/blink.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #define LED_BUILTIN 13
+extern uint64_t is_even(uint64_t num);
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -9,6 +10,7 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  is_even(123);
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
