17de68c9b4608828175a968d80efd860e8838702
luisMolina95 <luisrodmolina@gmail.com>
2025-09-12 14:37:52 -0600

    Add project 1

--- 14 EdJ/7/projects/1.asm
+++ 14 EdJ/7/projects/1.asm
@@ -5,9 +5,9 @@
 ;   1. bAns1 = bNum1 + bNum2
 ;   2. bAns2 = bNum1 + bNum3
 ;   3. bAns3 = bNum3 + bNum4
-;   4. bAns6 = bNum1 – bNum2
-;   5. bAns7 = bNum1 – bNum3
-;   6. bAns8 = bNum2 – bNum4
+;   4. bAns6 = bNum1 - bNum2
+;   5. bAns7 = bNum1 - bNum3
+;   6. bAns8 = bNum2 - bNum4
 ;   7. wAns11 = bNum1 * bNum3
 ;   8. wAns12 = bNum2 * bNum2
 ;   9. wAns13 = bNum2 * bNum4
@@ -18,9 +18,26 @@
 ; Use the debugger to execute the program and display the final results.
 ; Create a debugger input file to show the results in both decimal and hexadecimal.
 section .data
+; Answers:
 bAns1 db 0
-bNum1 db 1
-bNum2 db 2
+bAns2 db 0
+bAns3 db 0
+bAns6 db 0
+bAns7 db 0
+bAns8 db 0
+wAns11 dw 0
+wAns12 dw 0
+wAns13 dw 0
+bAns16 db 0
+bAns17 db 0
+bAns18 db 0
+bRem18 db 0
+; Numbers:
+bNum1 db 7
+bNum2 db 5
+bNum3 db 4
+bNum4 db 6
+wNum1 dw 303
 EXIT_SUCCESS equ 0
 SYS_exit     equ 60
 section .text
@@ -30,6 +47,60 @@ _start:
     mov al, byte [bNum1]
     add al, byte [bNum2]
     mov byte [bAns1], al
+;   2. bAns2 = bNum1 + bNum3
+    mov al, byte [bNum1]
+    add al, byte [bNum3]
+    mov byte [bAns2], al
+;   3. bAns3 = bNum3 + bNum4
+    mov al, byte [bNum3]
+    add al, byte [bNum4]
+    mov byte [bAns3], al
+;   4. bAns6 = bNum1 - bNum2
+    mov al, byte [bNum1]
+    sub al, byte [bNum2]
+    mov byte [bAns6], al
+;   5. bAns7 = bNum1 - bNum3
+    mov al, byte [bNum1]
+    sub al, byte [bNum3]
+    mov byte [bAns7], al
+;   6. bAns8 = bNum2 - bNum4
+    mov al, byte [bNum2]
+    sub al, byte [bNum4]
+    mov byte [bAns8], al
+;   7. wAns11 = bNum1 * bNum3
+    mov al, byte [bNum1]
+    mul byte [bNum3]
+    mov byte [wAns11], al
+    mov byte [wAns11 + 1], ah
+;   8. wAns12 = bNum2 * bNum2
+    mov al, byte [bNum2]
+    mul al
+    mov byte [wAns12], al
+    mov byte [wAns12 + 1], ah
+;   9. wAns13 = bNum2 * bNum4
+    mov al, byte [bNum2]
+    mul byte [bNum4]
+    mov byte [wAns13], al
+    mov byte [wAns13 + 1], ah
+;   10. bAns16 = bNum1 / bNum2
+    mov al, byte [bNum1]
+    div byte [bNum2]
+    mov byte [bAns16], al
+    mov ax, 0
+;   11. bAns17 = bNum3 / bNum4
+    mov al, byte [bNum3]
+    div byte [bNum4]
+    mov byte [bAns17], al
+    mov ax, 0
+;   12. bAns18 = wNum1 / bNum4
+    ; mov al, byte [wNum1]
+    ; mov ah, byte [wNum1 + 1]
+    mov ax, word [wNum1]
+    div byte [bNum4]
+    mov byte [bAns18], al
+;   13. bRem18 = wNum1 % bNum4
+    mov byte [bRem18], ah
+    mov ax, 0
 last:
     mov rax, SYS_exit
     mov rdi, EXIT_SUCCESS
--- 14 EdJ/7/projects/1.gdb
+++ 14 EdJ/7/projects/1.gdb
@@ -1,1 +1,80 @@
+break last
+run
+
+# 1. bAns1 = bNum1 + bNum2
+echo 1. bAns1 = bNum1 + bNum2\n
+printf "1. %05u = %05u + %05u\n", (unsigned char)bAns1, (unsigned char)bNum1, (unsigned char)bNum2
 x/ub &bAns1
+echo \n
+
+# 2. bAns2 = bNum1 + bNum3
+echo 2. bAns2 = bNum1 + bNum3\n
+printf "2. %05u = %05u + %05u\n", (unsigned char)bAns2, (unsigned char)bNum1, (unsigned char)bNum3
+x/ub &bAns2
+echo \n
+
+# 3. bAns3 = bNum3 + bNum4
+echo 3. bAns3 = bNum3 + bNum4\n
+printf "3. %05u = %05u + %05u\n", (unsigned char)bAns3, (unsigned char)bNum3, (unsigned char)bNum4
+x/ub &bAns3
+echo \n
+
+# 4. bAns6 = bNum1 - bNum2
+echo 4. bAns6 = bNum1 - bNum2\n
+printf "4. %05u = %05u - %05u\n", (unsigned char)bAns6, (unsigned char)bNum1, (unsigned char)bNum2
+x/ub &bAns6
+echo \n
+
+# 5. bAns7 = bNum1 - bNum3
+echo 5. bAns7 = bNum1 - bNum3\n
+printf "5. %05u = %05u - %05u\n", (unsigned char)bAns7, (unsigned char)bNum1, (unsigned char)bNum3
+x/ub &bAns7
+echo \n
+
+# 6. bAns8 = bNum2 - bNum4
+echo 6. bAns8 = bNum2 - bNum4\n
+printf "6. %05u = %05u - %05u\n", (unsigned char)bAns8, (unsigned char)bNum2, (unsigned char)bNum4
+x/ub &bAns8
+echo \n
+
+# 7. wAns11 = bNum1 * bNum3
+echo 7. wAns11 = bNum1 * bNum3\n
+printf "7. %05u = %05u * %05u\n", (unsigned short)wAns11, (unsigned char)bNum1, (unsigned char)bNum3
+x/uh &wAns11
+echo \n
+
+# 8. wAns12 = bNum2 * bNum2
+echo 8. wAns12 = bNum2 * bNum2\n
+printf "8. %05u = %05u * %05u\n", (unsigned short)wAns12, (unsigned char)bNum2, (unsigned char)bNum2
+x/uh &wAns12
+echo \n
+
+# 9. wAns13 = bNum2 * bNum4
+echo 9. wAns13 = bNum2 * bNum4\n
+printf "9. %05u = %05u * %05u\n", (unsigned short)wAns13, (unsigned char)bNum2, (unsigned char)bNum4
+x/uh &wAns13
+echo \n
+
+# 10. bAns16 = bNum1 / bNum2
+echo 10. bAns16 = bNum1 / bNum2\n
+printf "10. %05u = %05u / %05u\n", (unsigned char)bAns16, (unsigned char)bNum1, (unsigned char)bNum2
+x/ub &bAns16
+echo \n
+
+# 11. bAns17 = bNum3 / bNum4
+echo 11. bAns17 = bNum3 / bNum4\n
+printf "11. %05u = %05u / %05u\n", (unsigned char)bAns17, (unsigned char)bNum3, (unsigned char)bNum4
+x/ub &bAns17
+echo \n
+
+# 12. bAns18 = wNum1 / bNum4
+echo 12. bAns18 = wNum1 / bNum4\n
+printf "12. %05u = %05u / %05u\n", (unsigned char)bAns18, (unsigned short)wNum1, (unsigned char)bNum4
+x/ub &bAns18
+echo \n
+
+# 13. bRem18 = wNum1 % bNum4
+echo 13. bRem18 = wNum1 % bNum4\n
+printf "13. %05u = %05u %% %05u\n", (unsigned char)bAns18, (unsigned short)wNum1, (unsigned char)bNum4
+x/ub &bRem18
+echo \n
