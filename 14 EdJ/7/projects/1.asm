781f8245be2531b0cd061f89eca82b2e7db330ab
luisMolina95 <luisrodmolina@gmail.com>
2025-09-12 12:10:32 -0600

    First sum

--- 14 EdJ/7/projects/1.asm
+++ 14 EdJ/7/projects/1.asm
@@ -1,6 +1,7 @@
 ; Create a program to compute the following expressions using unsigned byte
-; variables and unsigned operations. Note, the first letter of the variable name
-; denotes the size (b → byte and w → word).
+; variables and unsigned operations.
+; Note, the first letter of the variable name denotes the size
+; (b → byte and w → word).
 ;   1. bAns1 = bNum1 + bNum2
 ;   2. bAns2 = bNum1 + bNum3
 ;   3. bAns3 = bNum3 + bNum4
@@ -14,13 +15,22 @@
 ;   11. bAns17 = bNum3 / bNum4
 ;   12. bAns18 = wNum1 / bNum4
 ;   13. bRem18 = wNum1 % bNum4
-; Use the debugger to execute the program and display the final results. Create
+; Use the debugger to execute the program and display the final results.
+; Create a debugger input file to show the results in both decimal and hexadecimal.
 section .data
+bAns1 db 0
+bNum1 db 1
+bNum2 db 2
 EXIT_SUCCESS equ 0
 SYS_exit     equ 60
 section .text
 global _start
 _start:
+;   1. bAns1 = bNum1 + bNum2
+    mov al, byte [bNum1]
+    add al, byte [bNum2]
+    mov byte [bAns1], al
+last:
     mov rax, SYS_exit
     mov rdi, EXIT_SUCCESS
     syscall
