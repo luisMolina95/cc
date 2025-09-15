ba04a0972cf14df523b6a6b196cdf3b409ac584a
luisMolina95 <luisrodmolina@gmail.com>
2025-09-15 14:10:09 -0600

    add test

--- 18 - node c plugin/windows/install.cmd
+++ 18 - node c plugin/windows/install.cmd
@@ -1,3 +1,4 @@
 @REM Working on node-gyp@11.4.2
-npm install --no-package-lock --loglevel silly
-npm run --loglevel silly install
+call npm install --no-save node-gyp
+call .\node_modules\.bin\node-gyp -v
+call .\node_modules\.bin\node-gyp install --loglevel=silly --devdir=./node-gyp
--- 18 - node c plugin/windows/src/addon.cc
+++ 18 - node c plugin/windows/src/addon.cc
@@ -2,7 +2,7 @@
 #include <node.h>
 
 void Initialize(v8::Local<v8::Object> exports){
-    std::cout << "Hi!" << std::endl;
+    std::cout << "Hi From C++!" << std::endl;
 }
 
 NODE_MODULE(MODE_GYP_MODULE_NAME, Initialize)
