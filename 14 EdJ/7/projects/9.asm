9bc2895eafbbb87ea4bbd170c89c731c0117477d
luisMolina95 <luisrodmolina@gmail.com>
2025-09-25 14:32:02 -0600

    Add initial 9

---
+++ 14 EdJ/7/projects/9.asm
@@ -0,0 +1,31 @@
+; Create a program to iteratively find the nth Fibonacci number. The value for n
+; should be set as a parameter (e.g., a programmer defined constant).
+; The formula for computing Fibonacci is as follows:
+;                / n                          if n = 0 or n = 1
+; fibonacci(n) = | fibonacci(n - 2) + 
+;                \          fibonacci(n - 1)  if n ≥ 2
+; Use the debugger to execute the program and display the final results.
+; Test the program for various values of n.
+; Create a debugger input file to show the results in both decimal and hexadecimal.
+section .data
+n dd 2
+result dd 0
+section .text
+global _start
+_start:
+    mov eax, 0
+    mov ebx, 1
+    mov edx, 0
+    mov ecx, DWORD [n]
+fibonacciLoop:
+    add edx, eax
+    add edx, ebx
+    mov eax, ebx
+    mov ebx, edx
+    mov edx, 0
+    loop fibonacciLoop
+    mov DWORD [result], eax
+last:
+    mov rax, 60
+    mov rdi, 0
+    syscall
---
+++ 14 EdJ/7/projects/9.gdb
@@ -0,0 +1,5 @@
+break last
+run
+echo Result:\n
+x/uw &result
+x/xw &result
